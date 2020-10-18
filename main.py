9349b88b117263ed31c107059b11de1ff7ed6b12
Lingxiang "LinG" Wang <lingxiang.wang@bytedance.com>
2020-10-17 22:23:10 -0400

    change to pysimplegui
    Change-Id: I648def6e8b91cc5412ae91dd0f44f70e722b2a91

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import wx
+import PySimpleGUI as sg
 
+# Define the window's contents
+layout = [[sg.Text("What's your name?")],  # Part 2 - The Layout
+          [sg.Input()],
+          [sg.Button('Ok')]]
+
+# Create the window
+window = sg.Window('Window Title', layout)  # Part 3 - Window Defintion
+
+# Display and interact with the Window
+event, values = window.read()  # Part 4 - Event loop or Window.read call
+
+# Do something with the information gathered
+print('Hello', values[0], "! Thanks for trying PySimpleGUI")
+
+# Finish up by removing from the screen
+window.close()  # Part 5 - Close the Window
